4051fa3ef5e4bb84f539b555d321613af84013ad
macbookUser <macbookuser@macbookpro-10.local>
2017-11-06 16:51:04 -0600

    Avance

--- Loggin/ViewController.swift
+++ Loggin/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        
+        logginButton.layer.cornerRadius = 5 //Le da radio a los bordes del botón "Iniciar sesión"
+      //  newCuentaButton.layer.cornerRadius = 5
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var logginButton: UIButton!
+    @IBAction func newCuentaButton(_ sender: UIButton) {
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
 }
 
